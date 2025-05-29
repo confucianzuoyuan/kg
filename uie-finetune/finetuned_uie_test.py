cea554f2bf8b82697653c1f8c6595892a132f60d
confucianzuoyuan <zuoyuantc@126.com>
2025-05-29 16:31:23 +0800

    update

--- uie-finetune/finetuned_uie_test.py
+++ uie-finetune/finetuned_uie_test.py
@@ -6,4 +6,4 @@ model = AutoModel.from_pretrained("./checkpoint/model_best", trust_remote_code=T
 
 schema = ['颜色', '商品名称', {'商品名称': '颜色'}]
 model.set_schema(schema)
-pprint(model.predict(tokenizer, "小米12S Ultra 骁龙8+旗舰处理器 徕卡光学镜头 2K超视感屏 120Hz高刷 67W快充 12GB+512GB 冷杉绿 5G手机"))
+pprint(model.predict(tokenizer, "小米12S Ultra 骁龙8+旗舰处理器 徕卡光学镜头 2K超视感屏 120Hz高刷 67W快充 12GB+512GB 玫瑰金 5G手机"))
